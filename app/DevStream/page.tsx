dbc4329d795d6c50f0b9223543ce2e207c143adf
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-09-01 12:18:27 +0300

    Add Carousel NPM

--- app/DevStream/page.tsx
+++ app/DevStream/page.tsx
@@ -1,8 +1,27 @@
 "use client";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import React from "react";
 import MoviesGrid from "@/src/components/MoviesGrid/MoviesGrid";
 import StreamList from "@/src/components/StreamList/StreamList";
 import LeadingVideo from "@/src/components/LeadingVideo/LeadingVideo";
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 export default function DevStreamHub() {
   const [books, setBooks] = React.useState<any>(DATA);
   function toggleBook(toggledBook: any) {
@@ -30,6 +49,24 @@ export default function DevStreamHub() {
         <StreamList books={selectedBooks} handleRemoveBook={toggleBook} />
       )} */}
       {/* </div> */}
+      <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+        className="w-[240px] h-[240px]"
+      >
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+      ;
     </main>
   );
 }

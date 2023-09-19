4d4ef1e93ecd71e6255e700743c139fee45c6091
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-09-19 12:21:17 +0300

    try this

--- app/blog/[postSlug]/page.tsx
+++ app/blog/[postSlug]/page.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import { loadBlogPost } from "../../../src/helpers/file-helper";
-
 async function BlogPostPage({ params }: any) {
-  const { content } = await loadBlogPost("about");
-
   return (
     <article>
       <div>

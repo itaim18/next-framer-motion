4f7ea6447691f458acd39a038eeacbb26b2f2baf
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-09-19 14:01:37 +0300

    try this

--- app/blog/[postSlug]/page.tsx
+++ app/blog/[postSlug]/page.tsx
@@ -1,22 +1,13 @@
 import { compileMDX } from "next-mdx-remote/rsc";
 import fs from "fs/promises";
 import path from "path";
-import matter from "gray-matter";
 import React from "react";
 export default async function Home({ params }: any) {
-  // Optionally provide a type for your frontmatter object
   const myPath = path.join(process.cwd(), `/content/${params.postSlug}.mdx`);
-  console.log(myPath);
   const res = await fs.readFile(myPath, "utf8");
-  console.log(res);
 
   const { content, frontmatter } = await compileMDX<{ title: string }>({
-    source: `---
-      title: RSC Frontmatter Example
-      ---
-      # Hello World
-      This is from Server Components!
-    `,
+    source: res,
     options: { parseFrontmatter: true },
   });
   return (

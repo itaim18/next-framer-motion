a956b77fceddc3768a140b2ea24d446765f26fd0
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-09-22 13:13:23 +0300

    remain hero

--- app/blog/[postSlug]/page.tsx
+++ app/blog/[postSlug]/page.tsx
@@ -4,7 +4,7 @@ import path from "path";
 import React from "react";
 // import MDXImage from "@/src/components/MDXImage/MDXImage";
 // // import PixelatedApple from "@/src/components/PixelatedApple/PixelatedApple";
-// import BlogHero from "@/src/components/BlogHero/BlogHero";
+import BlogHero from "@/src/components/BlogHero/BlogHero";
 // import { MDXRemote } from "next-mdx-remote/rsc";
 // import CodeSnippet from "@/src/components/CodeSnippet/CodeSnippet";
 import Strong from "@/src/components/Strong/Strong";
@@ -51,17 +51,18 @@ export async function generateMetadata({ params }: any) {
   };
 }
 
+const remarkHeading = (slug: any) => {
+  return slug
+    .toString()
+    .toLowerCase()
+    .replace(/[^a-z0-9-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
+
 async function Home({ params }: any) {
   const myPath = path.join(process.cwd(), `/content/${params.postSlug}.mdx`);
   const res = await fs.readFile(myPath, "utf8");
   const { content: myContent } = matter(res);
-  // const remarkHeading = (slug: any) => {
-  //   return slug
-  //     .toString()
-  //     .toLowerCase()
-  //     .replace(/[^a-z0-9-]+/g, "-")
-  //     .replace(/^-+|-+$/g, "");
-  // };
 
   const { frontmatter } = await compileMDX<any>({
     source: res,
@@ -72,12 +73,12 @@ async function Home({ params }: any) {
   return (
     <>
       <article className="my-36 relative max-w-2xl m-auto py-24">
-        {/* <BlogHero
+        <BlogHero
           title={frontmatter.title}
           publishedOn={frontmatter.publishedOn}
         />
         <div className="relative max-w-2xl text-lg m-auto p-8">
-          <MDXRemote
+          {/* <MDXRemote
             source={myContent}
             components={{
               pre: CodeSnippet,
@@ -119,8 +120,8 @@ async function Home({ params }: any) {
                 <p {...props} className="text-lg mt-6 mb-4 " />
               ),
             }}
-          />
-        </div> */}
+          /> */}
+        </div>
         <h1>{params.postSlug}</h1>
         <p className="border-t-2 text-lg leading-8 p-10 text-justify font-extrabold">
           Hey there! 👋 I&apos;m{" "}

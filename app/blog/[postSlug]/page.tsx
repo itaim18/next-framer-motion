24dfeff1543dfad0621f94e08fdb6d14429de3c0
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-09-22 12:27:42 +0300

    fix small issues

--- app/blog/[postSlug]/page.tsx
+++ app/blog/[postSlug]/page.tsx
@@ -13,43 +13,43 @@ import matter from "gray-matter";
 import SpecialLink from "@/src/components/SpecialLink/SpecialLink";
 import TableOfContents from "@/src/components/TableOfContents/TableOfContents";
 
-export async function generateMetadata({ params }: any) {
-  const myPath = path.join(process.cwd(), `/content/${params.postSlug}.mdx`);
-  const res = await fs.readFile(myPath, "utf8");
-  const { frontmatter } = await compileMDX<any>({
-    source: res,
-    options: { parseFrontmatter: true },
-  });
+// export async function generateMetadata({ params }: any) {
+//   const myPath = path.join(process.cwd(), `/content/${params.postSlug}.mdx`);
+//   const res = await fs.readFile(myPath, "utf8");
+//   const { frontmatter } = await compileMDX<any>({
+//     source: res,
+//     options: { parseFrontmatter: true },
+//   });
 
-  return {
-    title: `${frontmatter.title} • Blog`,
-    description: frontmatter.abstract,
-    openGraph: {
-      type: "website",
-      locale: "en_US",
-      title: "Itai Mizlish's Blog",
-      description: frontmatter.abstract,
-      siteName: "Itai Mizlish's Blog",
-      images: [
-        {
-          url: `https://raw.githubusercontent.com/itaim18/next-framer-motion/main/public/assets/${params.postSlug}.png`,
-          width: 1920,
-          height: 1080,
-          alt: params.postSlug,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Itai Mizlish's Blog",
-      description: frontmatter.abstract,
-      images: [
-        `https://raw.githubusercontent.com/itaim18/next-framer-motion/main/public/assets/${params.postSlug}.png`,
-      ],
-      creator: "@IMizlish",
-    },
-  };
-}
+//   return {
+//     title: `${frontmatter.title} • Blog`,
+//     description: frontmatter.abstract,
+//     openGraph: {
+//       type: "website",
+//       locale: "en_US",
+//       title: "Itai Mizlish's Blog",
+//       description: frontmatter.abstract,
+//       siteName: "Itai Mizlish's Blog",
+//       images: [
+//         {
+//           url: `https://raw.githubusercontent.com/itaim18/next-framer-motion/main/public/assets/${params.postSlug}.png`,
+//           width: 1920,
+//           height: 1080,
+//           alt: params.postSlug,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title: "Itai Mizlish's Blog",
+//       description: frontmatter.abstract,
+//       images: [
+//         `https://raw.githubusercontent.com/itaim18/next-framer-motion/main/public/assets/${params.postSlug}.png`,
+//       ],
+//       creator: "@IMizlish",
+//     },
+//   };
+// }
 
 export default async function Home({ params }: any) {
   const myPath = path.join(process.cwd(), `/content/${params.postSlug}.mdx`);
